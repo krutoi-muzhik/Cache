f748bee649e1e4f021f921a61343859054147957
krutoi-muzhik <ddaniild@list.ru>
2023-09-25 02:13:26 +0300

    LRU cache v1

---
+++ LRU.cc
@@ -0,0 +1,13 @@
+#include "LRU.hpp"
+
+int main() {
+	LRU c(4);
+
+	c.refer(1);
+	c.refer(2);
+	c.refer(3);
+	c.refer(1);
+	c.refer(4);
+	c.refer(5);
+	c.display();
+}
---
+++ LRU.hpp
@@ -0,0 +1,52 @@
+#ifndef LRU_H_
+
+#define LRU_H_
+
+#include <list>
+#include <cstdlib>
+#include <unordered_map>
+#include <iostream>
+
+typedef int page_t;
+
+class LRU {
+private:
+	size_t sz_;
+	std::list<page_t> L;
+	using ListIt = std::list<page_t>::iterator;
+	ListIt It;
+	std::unordered_map<page_t, ListIt> M;
+	using MapIt = std::unordered_map<page_t, ListIt>::iterator;
+
+public:
+	LRU(size_t sz): sz_(sz) {}
+	
+	~LRU() {}
+	
+	bool refer(page_t page) {
+		bool found = false;
+		if (M.find(page) == M.end()) {
+			if (L.size() == sz_) {
+				page_t last = L.back();
+				L.pop_back();
+				M.erase(last);
+			}
+		} else {
+			found = true;
+			L.erase(M[page]);
+		}
+		L.push_front(page);
+		M[page] = L.begin();
+		return found;
+	}
+
+	void display() {
+		for (ListIt it = L.begin(); it != L.end(); it ++) {
+			std::cout << (*it) << " ";
+		}
+		std::cout << std::endl;
+	}
+};
+
+
+#endif			// LRU_H_
