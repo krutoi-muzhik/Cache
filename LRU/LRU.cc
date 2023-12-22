a36b215b430b83dc16e85f3bdfc6d44a27f55768
Daniil Dementev <ddaniild@list.ru>
2023-12-22 03:03:51 +0300

    refactored

--- LRU/LRU.cc
+++ LRU/LRU.cc
@@ -1,13 +1,13 @@
 #include "LRU.hpp"
 
 int main() {
-	LRU<int> c(4);
+	LRU<int, int> c(4);
 
-	c.refer(1);
-	c.refer(2);
-	c.refer(3);
-	c.refer(1);
-	c.refer(4);
-	c.refer(5);
+	c.refer(1, 1);
+	c.refer(2, 2);
+	c.refer(3, 3);
+	c.refer(1, 1);
+	c.refer(4, 4);
+	c.refer(5, 5);
 	c.display();
 }
--- LRU/LRU.hpp
+++ LRU/LRU.hpp
@@ -8,45 +8,58 @@
 #include <iostream>
 
 
-template <typename page_t>
+template <typename page_num_t, typename page_t>
 class LRU {
 private:
 	size_t sz_;
 	std::list<page_t> L;
 	using ListIt = typename std::list<page_t>::iterator;
 	ListIt It;
-	std::unordered_map<page_t, ListIt> M;
-	using MapIt = typename std::unordered_map<page_t, ListIt>::iterator;
+	std::unordered_map<page_num_t, ListIt> M;
+	using MapIt = typename std::unordered_map<page_num_t, ListIt>::iterator;
 
 public:
 	LRU(size_t sz): sz_(sz) {}
-	
 	~LRU() {}
-	
-	bool refer(page_t page) {
-		bool found = false;
-		if (M.find(page) == M.end()) {
-			if (L.size() == sz_) {
-				page_t last = L.back();
-				L.pop_back();
-				M.erase(last);
-			}
-		} else {
-			found = true;
-			L.erase(M[page]);
-		}
-		L.push_front(page);
-		M[page] = L.begin();
-		return found;
-	}
-
-	void display() {
-		for (ListIt it = L.begin(); it != L.end(); it ++) {
-			std::cout << (*it) << " ";
-		}
-		std::cout << std::endl;
-	}
+	bool hit(page_num_t pn);
+	bool refer(page_num_t pn, page_t p);
+	void display();
 };
 
+template<typename page_num_t, typename page_t>
+bool LRU<page_num_t, page_t>::hit(page_num_t pn) {
+	if (M.find(pn) == M.end())
+		return false;
+
+	return true;
+}
+
+template<typename page_num_t, typename page_t>
+bool LRU<page_num_t, page_t>::refer(page_num_t pn, page_t p) {
+	bool hit_;
+	hit_ = hit(pn);
+
+	if (hit_) {
+		L.erase(M[pn]);
+	} else {
+		if (L.size() == sz_) {
+			M.erase(M.find(L.end()));
+			L.pop_back();
+		}
+	}
+
+	L.push_front(p);
+	M[pn] = L.begin();
+
+	return hit_;
+}
+
+template<typename page_num_t, typename page_t>
+void LRU<page_num_t, page_t>::display() {
+	for (ListIt it = L.begin(); it != L.end(); it ++) {
+		std::cout << (*it) << " ";
+	}
+	std::cout << std::endl;
+}
 
 #endif			// LRU_H_
